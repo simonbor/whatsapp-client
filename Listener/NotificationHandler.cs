f6e6ceec9ca06567cde8ed00517b31e254cca391
Simon <simonbor@gmail.com>
2020-10-20 01:21:19 +0300

    main refactor - notification handler

---
+++ Listener/NotificationHandler.cs
@@ -0,0 +1,142 @@
+﻿using Listener.Models;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Foundation.Metadata;
+using Windows.UI.Notifications;
+using Windows.UI.Notifications.Management;
+
+namespace Listener
+{
+    public class NotificationHandler
+    {
+        static SemaphoreSlim semaphoreSlim = new SemaphoreSlim(1, 1);
+        private static readonly UserNotificationListener listener = UserNotificationListener.Current;
+        private static readonly Config.AppConfig config = new Config.AppConfig();
+
+        public void SubscribeToWindowsNotifications()
+        {
+            listener.NotificationChanged += Listener_NotificationChanged;
+        }
+
+        private async Task<bool> ProcessNotification(WaNotification waNotification)
+        {
+            var request = Helpers.GetRequest(waNotification);
+            var client = new HttpClient { BaseAddress = new Uri(config.Server.Url) };
+            var content = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
+
+            if (!config.App.DryRun)
+            {
+                var result = await client.PostAsync("chance", content);
+                var resultJsonString = await result.Content.ReadAsStringAsync();
+                var resultBody = JsonConvert.DeserializeObject<ChanceRes>(resultJsonString);
+
+                if (result.StatusCode != System.Net.HttpStatusCode.OK)
+                {
+                    ConsoleProxy.WriteLine(null, ConsoleColor.DarkRed, 
+                        $"Error has ocurred while sending WA notification: {waNotification.Id.ToString()}, HttpStatus is: {result.StatusCode}");
+                    return false;
+                }
+
+                if (resultBody.StatusCode != 200)
+                {
+                    ConsoleProxy.WriteLine(null, ConsoleColor.Red, 
+                        $"Error has ocurred while processing WA notification: {waNotification.Id.ToString()}, " +
+                        $"StatusCode: '{resultBody.StatusCode}', StatusText: '{resultBody.StatusText}'");
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public async void PerformNotifications(object autoEvent)
+        {
+            await semaphoreSlim.WaitAsync(); // working as a lock (){}
+            try
+            {
+                var userNotifications = await listener.GetNotificationsAsync(NotificationKinds.Toast);
+
+                foreach (UserNotification userNotification in userNotifications)
+                {
+                    var waNotification = new WaNotification(userNotification);
+                    if (Helpers.IsValid(waNotification))
+                    {
+                        var passed = await ProcessNotification(waNotification);
+                        if (passed)
+                        {
+                            listener.RemoveNotification(userNotification.Id);
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                semaphoreSlim.Release();
+            }
+        }
+
+        private async void Listener_NotificationChanged(UserNotificationListener sender, UserNotificationChangedEventArgs args)
+        {
+            var userNotifications = await listener.GetNotificationsAsync(NotificationKinds.Toast);
+
+            foreach (UserNotification userNotification in userNotifications)
+            {
+                if (userNotification.Id == args.UserNotificationId)
+                {
+                    var waNotification = new WaNotification(userNotification);
+
+                    ConsoleProxy.WriteLine(null, ConsoleColor.Yellow, $"Notification arrived " +
+                        $"at: {DateTime.Now.ToLocalTime().ToShortTimeString()}, id: {args.UserNotificationId}");
+                    if (Helpers.IsValid(waNotification))
+                    {
+                        var passed = await ProcessNotification(waNotification);
+                        if (passed)
+                        {
+                            listener.RemoveNotification(userNotification.Id);
+                        }
+                    }
+                }
+            }
+        }
+
+        public async Task<bool> RequestNotificationListenerAccess()
+        {
+            // Check whether the listener is supported
+            if (ApiInformation.IsTypePresent("Windows.UI.Notifications.Management.UserNotificationListener"))
+            {
+                try
+                {
+                    // request access to the user's notifications (must be called from UI thread) or
+                    // just turn Notifications On at Windows Apps & Features -> Listener -> App Permissions
+                    UserNotificationListenerAccessStatus accessStatus = await listener.RequestAccessAsync();
+                    if (accessStatus != UserNotificationListenerAccessStatus.Allowed)
+                    {
+                        ConsoleProxy.WriteLine(null, ConsoleColor.DarkRed, "Notification listener access isn't allowed in Windows settings");
+                        return false;
+                    }
+                }
+                catch (Exception e)
+                {
+                    ConsoleProxy.WriteLine(null, ConsoleColor.DarkRed, e.Message);
+                }
+
+                return true;
+            }
+            else
+            {
+                ConsoleProxy.WriteLine(null, ConsoleColor.DarkRed, "The Windows version is not compatible - Notification Listener is not exists");
+            }
+
+            return false;
+        }
+
+        public bool ToastNotificationActionTriggerIsPresent()
+        {
+            return ApiInformation.IsTypePresent("Windows.ApplicationModel.Background.ToastNotificationActionTrigger");
+        }
+    }
+}
--- Listener/Program.cs
+++ Listener/Program.cs
@@ -1,13 +1,6 @@
-﻿using Listener.Models;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
+﻿using System;
 using System.Text;
-using System.Threading.Tasks;
-using Windows.Foundation.Metadata;
-using Windows.UI.Notifications;
-using Windows.UI.Notifications.Management;
+using System.Threading;
 
 // https://blog.pieeatingninjas.be/2018/04/05/creating-a-uwp-console-app-in-c/
 // https://docs.microsoft.com/en-us/windows/uwp/design/shell/tiles-and-notifications/notification-listener
@@ -15,101 +8,48 @@ namespace Listener
 {
     class Program
     {
-        private static readonly UserNotificationListener listener = UserNotificationListener.Current;
         private static readonly Config.AppConfig config = new Config.AppConfig();
-
-        static async void Listener_NotificationChanged(UserNotificationListener sender, UserNotificationChangedEventArgs args)
-        {
-            var userNotifications = await sender.GetNotificationsAsync(NotificationKinds.Toast);
-
-            foreach (UserNotification userNotification in userNotifications)
-            {
-                if (userNotification.Id == args.UserNotificationId)
-                {
-                    var waNotification = new WaNotification(userNotification);
-
-                    ConsoleProxy.WriteLine(null, ConsoleColor.Yellow, $"Notification arrived " +
-                        $"at: {DateTime.Now.ToLocalTime().ToShortTimeString()}, id: {args.UserNotificationId}");
-                    if (Helpers.IsValid(waNotification))
-                    {
-                        await ProcessNotification(waNotification);
-                    }
-                }
-            }
-        }
 
         static void Main(string[] args)
         {
+            var notificationHandler = new NotificationHandler();
             Console.OutputEncoding = Encoding.Unicode;
 
-            // check for exists notifications
-            ConsoleProxy.WriteLine(null, ConsoleColor.Gray, "Check for exists notifications");
-            if (RequestAccess(listener).Result)
+            if(!notificationHandler.ToastNotificationActionTriggerIsPresent())
             {
-                var userNotifications = GetNotifications(listener).Result;
-
-                foreach (UserNotification userNotification in userNotifications)
-                {
-                    var waNotification = new WaNotification(userNotification);
-                    if (Helpers.IsValid(waNotification))
-                    {
-                        ProcessNotification(waNotification).Wait();
-                    }
-                }
+                Console.WriteLine("ToastNotificationActionTrigger isn't present. Check Windows settings.");
+                return;
             }
 
-            // wait for new notifications
-            ConsoleProxy.WriteLine(null, ConsoleColor.Gray, "\nWaiting for new norifications...");
-            listener.NotificationChanged += Listener_NotificationChanged;
-
-            Console.ReadLine();
-        }
-
-        static async Task ProcessNotification(WaNotification waNotification)
-        {
-            var request = Helpers.GetRequest(waNotification);
-            var client = new HttpClient { BaseAddress = new Uri(config.Server.Url) };
-            var content = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
-
-            if (!config.App.Dryrun)
+            if (notificationHandler.RequestNotificationListenerAccess().Result)
             {
-                var result = await client.PostAsync("chance", content);
-
-                if (result.StatusCode != System.Net.HttpStatusCode.OK)
-                {
-                    ConsoleProxy.WriteLine(null, ConsoleColor.DarkRed, $"Error has ocurred while sending WA notification: {waNotification.Id.ToString()}, HttpStatus is: {result.StatusCode}");
-                }
+                notificationHandler.PerformNotifications(null);
             }
-        }
 
-        static async Task<IReadOnlyList<UserNotification>> GetNotifications(UserNotificationListener listener)
-        {
-            // Get all notifications from the platform
-            return await listener.GetNotificationsAsync(NotificationKinds.Toast);
-        }
-
-        static async Task<bool> RequestAccess(UserNotificationListener listener)
-        {
-            // Check whether the listener is supported
-            if (ApiInformation.IsTypePresent("Windows.UI.Notifications.Management.UserNotificationListener"))
+            if (config.App.CheckForNotificationPeriodically)
             {
-                // request access to the user's notifications (must be called from UI thread) or
-                // just turn Notifications On at Windows Apps & Features -> Listener -> App Permissions
-                UserNotificationListenerAccessStatus accessStatus = await listener.RequestAccessAsync();
+                int delay = config.App.CheckForNotificationPeriod;
+                ConsoleProxy.WriteLine(null, ConsoleColor.Yellow, $"Checking for notifications in loop every {delay / 1000} sec.");
+                var timer = new Timer(notificationHandler.PerformNotifications, null, delay, delay);
 
-                if(accessStatus != UserNotificationListenerAccessStatus.Allowed)
-                {
-                    ConsoleProxy.WriteLine(null, ConsoleColor.DarkRed, "Notification listener access isn't allowed in Windows settings");
-                }
-
-                return true;
+                Console.ReadLine();
+                Console.WriteLine("\nDestroying timer.");
+                timer.Dispose();
             }
             else
             {
-                ConsoleProxy.WriteLine(null, ConsoleColor.DarkRed, "The Windows version is not compatible - no Listener exists");
-            }
+                try
+                {
+                    notificationHandler.SubscribeToWindowsNotifications();
+                    ConsoleProxy.WriteLine(null, ConsoleColor.Gray, "\nWaiting for new notifications...");
 
-            return false;
+                    Console.ReadLine();
+                }
+                catch (Exception e)
+                {
+                    ConsoleProxy.WriteLine(null, ConsoleColor.DarkRed, $"{e.Message}");
+                }
+            }
         }
     }
 }
